1ef20de251beaac16ac09c0506dfc7555ae4e909
Hamed <hamedgramzi@gmail.com>
2020-11-19 16:31:11 +0330

    fix touch bug

--- guidedkeyboardfixerlib/src/main/java/com/example/guidedkeyboardfixerlib/MyEditText.java
+++ guidedkeyboardfixerlib/src/main/java/com/example/guidedkeyboardfixerlib/MyEditText.java
@@ -1,20 +1,31 @@
 package com.example.guidedkeyboardfixerlib;
 
+import android.app.Instrumentation;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.leanback.widget.GuidedActionEditText;
 import androidx.leanback.widget.ImeKeyMonitor;
+import androidx.leanback.widget.VerticalGridView;
+
 import android.util.AttributeSet;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityNodeInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MyEditText extends EditText implements ImeKeyMonitor {
+import java.util.logging.Handler;
+
+public class MyEditText extends AppCompatEditText implements ImeKeyMonitor {
     static final class NoPaddingDrawable extends Drawable {
         @Override
         public boolean getPadding(Rect padding) {
@@ -80,6 +91,33 @@ public class MyEditText extends EditText implements ImeKeyMonitor {
     public void onInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info) {
         super.onInitializeAccessibilityNodeInfo(info);
         info.setClassName(isFocused() ? EditText.class.getName() : TextView.class.getName());
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        try {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                VerticalGridView verticalGridView = ((VerticalGridView) getParent().getParent().getParent());
+                int dest = verticalGridView.getChildAdapterPosition((View) getParent().getParent());
+                if (verticalGridView.getSelectedPosition() != dest)
+                    verticalGridView.setSelectedPositionSmooth(dest);
+
+                requestFocus();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Instrumentation inst = new Instrumentation();
+                            inst.sendKeyDownUpSync(KeyEvent.KEYCODE_NUMPAD_ENTER);
+                        } catch (Exception e) {
+                        }
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+
+        }
+        return super.dispatchTouchEvent(event);
     }
 
     @Override
